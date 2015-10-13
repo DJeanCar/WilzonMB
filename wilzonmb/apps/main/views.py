24ec3bcea1718416689913f08caa7bef3d911bb3
Jean Carlos Mariños Urquiaga <mjeanc.104@gmail.com>
2015-10-12 23:28:25 -0500

    templates ,yarchivos estaticos, mostrar datos

--- wilzonmb/apps/main/views.py
+++ wilzonmb/apps/main/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
+from apps.posts.models import Post
+
 class IndexView(TemplateView):
 
 	template_name = 'main/home.html'
+
+	def get_context_data(self, **kwargs):
+		context = super(IndexView, self).get_context_data(**kwargs)
+		context['posts'] = Post.objects.all().order_by('-created')
+		return context
